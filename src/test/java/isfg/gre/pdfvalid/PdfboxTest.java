1b31f4f3a5d0129401013d9770988cb83b3a34f1
Milan Gren <gre@celaeno>
2020-09-03 15:29:02 +0200

    saving

--- src/test/java/isfg/gre/pdfvalid/PdfboxTest.java
+++ src/test/java/isfg/gre/pdfvalid/PdfboxTest.java
@@ -13,6 +13,8 @@ import java.io.InputStream ;
 import isfg.gre.pdfvalid.service.PDFValidatorVERA ;
 
 // logger
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.*; // TODO HACK hnus hnus
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,11 +24,7 @@ import org.junit.Ignore ;
 import org.junit.Test;
 
 // 
-import org.apache.pdfbox.pdmodel.PDDocument ;
-import org.apache.pdfbox.pdmodel.PDPage ;
-import org.apache.pdfbox.pdmodel.PDPageContentStream ; 
-import org.apache.pdfbox.pdmodel.PDDocumentInformation;
-
+import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode ;
 import org.apache.pdfbox.pdmodel.font.PDType1Font ;
 import org.apache.xmpbox.XMPMetadata ;
 //import org.apache.jempbox.xmp.pdfa.XMPSchemaPDFAId ;
@@ -74,7 +72,7 @@ public class PdfboxTest {
     public void createPdf(String fontPath) throws java.io.IOException {
       PDDocument document = new PDDocument();    
 
-      for (int i=0; i<2; i++) {
+      for (int i=0; i<1; i++) {
         PDPage blankPage = new PDPage();
         document.addPage( blankPage );
       }    
@@ -109,64 +107,89 @@ public class PdfboxTest {
 
 
     public void createValidPDf(int flavourIdint, String startingPDFile,String fontPath) throws java.io.IOException, org.apache.xmpbox.type.BadFieldValueException, javax.xml.transform.TransformerException {
-    
-    
-      PDDocument doc ;
-      File file ;
-      if (true) {//load starting pdf file 
-        file = new File(startingPDFile); 
-        doc = PDDocument.load(file); 
-      } else { // start very new document 
-        doc = new PDDocument() ;
-      }
 
-      PDPage page = new PDPage();
-      doc.addPage(page);
-      PDFont font = PDType0Font.load(doc, new File(fontPath));
-	
-      String message = "ahoj ahoj ahoj"; 
+
+
+        PDDocument doc ;
+
+        File file ;
+        if (true) {//load starting pdf file
+            file = new File(startingPDFile);
+            doc = PDDocument.load(file);
+        } else { // start very new document
+          // doc = new PDDocument() ;
+        }
+
+        PDFont font = PDType0Font.load(doc, new File(fontPath));
+
+      // zmena fontu na strane 0
+        PDPage page0 = doc.getPage(0);
+
+
+        PDResources resources = page0.getResources();
+
+        for(COSName key : resources.getFontNames()) {
+            PDFont fnt = resources.getFont(key);
+            log.info(fnt.getFontDescriptor().getFontName());
+            //resources.put(key, font) ;
+        }
+
+        PDPageContentStream contents0 = new PDPageContentStream(doc, page0, AppendMode.APPEND, true, true) ;
+        contents0.beginText();
+        contents0.setFont(font, 18);
+        contents0.newLineAtOffset(55, 450);
+        String text = "PRIDANO" ;
+        contents0.showText(text);
+        contents0.endText();
+        contents0.close();
+
+        PDPage page = new PDPage();
+        doc.addPage(page);
+
+
+        String message = "ahoj ahoj ahoj";
  	            // load the font as this needs to be embedded
 
-      if (!font.isEmbedded()) {
-        throw new IllegalStateException("PDF/A compliance requires that all fonts used for text rendering in rendering modes other than rendering mode 3 are embedded.");
-      }
+        if (!font.isEmbedded()) {
+            throw new IllegalStateException("PDF/A compliance requires that all fonts used for text rendering in rendering modes other than rendering mode 3 are embedded.");
+        }
 
-      PDPageContentStream contents = new PDPageContentStream(doc, page) ;
-      contents.beginText();
-      contents.setFont(font, 12);
-      contents.newLineAtOffset(100, 700);
-      contents.showText(message);
-      contents.endText();
-      contents.close();  
-      
-      XMPMetadata xmp = XMPMetadata.createXMPMetadata();
-      PDFAIdentificationSchema id = xmp.createAndAddPFAIdentificationSchema();
-      id.setPart(flavourIdint);
-      id.setConformance("B");
+        PDPageContentStream contents = new PDPageContentStream(doc, page) ;
+        contents.beginText();
+        contents.setFont(font, 12);
+        contents.newLineAtOffset(100, 700);
+        contents.showText(message);
+        contents.endText();
+        contents.close();
 
-      XmpSerializer serializer = new XmpSerializer();
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      serializer.serialize(xmp, baos, true);
+        XMPMetadata xmp = XMPMetadata.createXMPMetadata();
+        PDFAIdentificationSchema id = xmp.createAndAddPFAIdentificationSchema();
+        id.setPart(flavourIdint);
+        id.setConformance("B");
 
-      PDMetadata metadata = new PDMetadata(doc);
-      metadata.importXMPMetadata(baos.toByteArray());
-      doc.getDocumentCatalog().setMetadata(metadata);
+        XmpSerializer serializer = new XmpSerializer();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        serializer.serialize(xmp, baos, true);
 
-      InputStream colorProfile = new FileInputStream(
+        PDMetadata metadata = new PDMetadata(doc);
+        metadata.importXMPMetadata(baos.toByteArray());
+        doc.getDocumentCatalog().setMetadata(metadata);
 
-      Paths.get("src","test","resources","Adobe_ICC_Profiles_end-user","RGB","AdobeRGB1998.icc").toFile().getAbsolutePath()) ;
+        InputStream colorProfile = new FileInputStream(
 
-      PDOutputIntent intent = new PDOutputIntent(doc, colorProfile);
+        Paths.get("src","test","resources","Adobe_ICC_Profiles_end-user","RGB","AdobeRGB1998.icc").toFile().getAbsolutePath()) ;
 
-      intent.setInfo("sRGB IEC61966-2.1");
-      intent.setOutputCondition("sRGB IEC61966-2.1");
-      intent.setOutputConditionIdentifier("sRGB IEC61966-2.1");
-      intent.setRegistryName("http://www.color.org");
-      doc.getDocumentCatalog().addOutputIntent(intent);
+        PDOutputIntent intent = new PDOutputIntent(doc, colorProfile);
 
-      doc.save(Paths.get("src","test","tmp","validated.pdf").toFile().getAbsolutePath()) ;
+        intent.setInfo("sRGB IEC61966-2.1");
+        intent.setOutputCondition("sRGB IEC61966-2.1");
+        intent.setOutputConditionIdentifier("sRGB IEC61966-2.1");
+        intent.setRegistryName("http://www.color.org");
+        doc.getDocumentCatalog().addOutputIntent(intent);
 
-      doc.close() ;
+        doc.save(Paths.get("src","test","tmp","validated.pdf").toFile().getAbsolutePath()) ;
+
+        doc.close() ;
         
     }
 
@@ -178,8 +201,7 @@ public class PdfboxTest {
     @Test
     public void TEST_IS_VALID() throws PDFValidationException, FileNotFoundException, java.io.IOException, org.apache.xmpbox.type.BadFieldValueException, javax.xml.transform.TransformerException {
     
-        createValidPDf(1,Paths.get("src","test","tmp","MYPDF.pdf").toFile().getAbsolutePath(),
-//                         "/home/gre/pdfbox/pdfbox/src/main/resources/org/apache/pdfbox/resources/ttf/LiberationSans-Regular.ttf"
+        createValidPDf(2,Paths.get("src","test","resources","fonts.pdf").toFile().getAbsolutePath(),
                                    Paths.get("src","test","resources","Pacifico.ttf").toFile().getAbsolutePath()
         ) ;
         
